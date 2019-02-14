12f522a6c3e61cea01a993510c7884091cd0c188
Maurício Spinardi <mauricio.spinardi@gmail.com>
2019-02-14 08:41:27 -0200

    Acrescenta esqueleto servidor Spring Boot Web, com suporte para base embarcada H2

---
+++ spring-boot-web-h2-server/src/main/java/com/sbwh2/springbootwebh2/SpringBootWebH2Application.java
@@ -0,0 +1,14 @@
+package com.sbwh2.springbootwebh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebH2Application.class, args);
+	}
+
+}
+
---
+++ spring-boot-web-h2-server/src/test/java/com/sbwh2/springbootwebh2/SpringBootWebH2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sbwh2.springbootwebh2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebH2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
