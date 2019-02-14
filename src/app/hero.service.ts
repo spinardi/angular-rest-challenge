2c5f360fdf6226340f45e755781a484735b10d8a
Maurício Spinardi <mauricio.spinardi@gmail.com>
2019-02-14 00:12:23 -0200

    Habilita HTTP, simula um servidor e atualiza HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,8 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
-import { MOCK_HEROES } from './mock-heroes';
-
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -11,11 +10,21 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  /* URL to web api */
 
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
+
+  /* Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /* GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(MOCK_HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
