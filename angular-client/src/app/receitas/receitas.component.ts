e4c5fd0a6ea29157886c34d1131fdf125c64e829
Maurício Spinardi <mauricio.spinardi@gmail.com>
2019-02-15 16:34:12 -0200

    Implementa listagem e mocka receitas

--- angular-client/src/app/receitas/receitas.component.ts
+++ angular-client/src/app/receitas/receitas.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { Receita } from '../receita';
 
+/* TODO: capturar da base de dados via servidor */
+import { RECEITAS } from '../mock-receitas';
+
 @Component({
   selector: 'app-receitas',
   templateUrl: './receitas.component.html',
@@ -9,22 +12,7 @@ import { Receita } from '../receita';
 })
 export class ReceitasComponent implements OnInit {
 
-  receita: Receita = {
-    id: 16,
-    nome: 'Bolo de cenoura',
-    calorias: 333,
-    ingrediente: [
-      '3 cenouras médias raspadas e picadas',
-      '3 ovos',
-      '1 xícara de óleo',
-      '2 xícaras de açúcar',
-      '2 xícaras de farinha de trigo',
-      '1 colher (sopa) de fermento em pó',
-      '1 pitada de sal'
-    ],
-    modoPreparo: 'Bater no liquidificador todos os ingredientes, ' +
-      'acrescentando a farinha aos poucos'
-  }
+  receitas = RECEITAS;
 
   constructor() { }
 
