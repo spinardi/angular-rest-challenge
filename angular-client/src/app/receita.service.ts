05dab63403fb2cfcf4d7f559f7cf4892cc7bc331
Maurício Spinardi <mauricio.spinardi@gmail.com>
2019-02-16 00:48:02 -0200

    Acrescenta log de serviços no front

--- angular-client/src/app/receita.service.ts
+++ angular-client/src/app/receita.service.ts
@@ -3,15 +3,17 @@ import { Observable, of } from 'rxjs';
 
 import { Receita } from './receita';
 import { RECEITAS } from './mock-receitas';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReceitaService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getReceitas(): Observable<Receita[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(RECEITAS);
   }
 }
